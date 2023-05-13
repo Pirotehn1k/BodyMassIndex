a23948dd36e9650987c4b9af5ffe54bb38b23efa
Yulian Akshov <AKsh4751@gmail.com>
2023-05-13 21:34:21 +0300

    fix gitignore, reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double meter, double kg ){
-        double bmi=kg/ (meter * meter);
+    public double calculate(double meter, double kg) {
+        double bmi = kg / (meter * meter);
         return bmi;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-public static void main (String[] args) {
-   BmiService service = new BmiService();
-   double kg=98;
-   double meter=1.87;
-   double bmi = service.calculate(meter,kg);
-   System.out.println((int)bmi);
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double kg = 98;
+        double meter = 1.87;
+        double bmi = service.calculate(meter, kg);
+        System.out.println((int) bmi);
 
-   }
+    }
 }
